385c5ddc2b7fe0c1a2b066920cdec2af2e441bce
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-09-15 20:10:53 -0400

    Welp look who is back two years later ME!!!!

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+/// view engine setup
+app.set('views', path.join(__dirname, 'pages'));
+/// css setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+/// load the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
